f5221a78f8766dfdf290629a18599cbbd673822b
StasShevelov <108366600+StasShevelov@users.noreply.github.com>
2025-07-31 18:58:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ initial_prompt = {
 
 def think(chat_history):
     response = client.chat.completions.create(
-        model="gpt-4.1-nano",
+        model="deepseek-r1"",
         messages=chat_history,
         web_search=False
     )
